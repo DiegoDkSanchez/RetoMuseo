d97fabad360c332e5288e2d6ef495e5914e35a60
Diegodk <diegodksanchez@gmail.com>
2018-04-16 10:31:00 -0600

    Primer Commit

---
+++ MuseoMarteCopia/MuseoMarteCopia/MainPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace MuseoMarteCopia
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+
+            InitializeComponent();
+
+            var tapGestureRecognizer = new TapGestureRecognizer();
+            tapGestureRecognizer.Tapped += (s, e) => {
+
+                Navigation.PushAsync(new Page1());
+                //DisplayAlert("Alert", "Activaste una imagen", "OK");
+
+            };
+            imagen1.GestureRecognizers.Add(tapGestureRecognizer);
+        }
+	}
+}
